fb1fda214acec18726f7f2b40c05b05630b6ceba
desafio-7-dnc <agenciadigitalupbr@gmail.com>
2024-02-26 00:39:38 -0300

    Deixo relatado ajuste das pasta e jest como pedido no desafio

---
+++ src/serve.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import produtoRoute from './src/module/produto/produto.route';
+const app = express();
+app.use(express.json());
+
+app.use('/produto', produtoRoute);
+
+app.listen(8080, () => {
+  console.log('Servidor funcionando!!');
+});
