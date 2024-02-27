18ec4e4ec3ce8b93aa9b3b4b93a5f2af35e5a580
desafio-7-dnc <agenciadigitalupbr@gmail.com>
2024-02-27 01:30:37 -0300

    Alterações feitas TS

--- src/module-produto/produto.route.ts
+++ src/module-produto/produto.route.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import produtoModel from './produto.model';
+import express from 'express'
+import produtoModel from './produto.model'
 
 const router = express.Router();
 
 router.get('/', async (_, res) => {
   const data = await produtoModel.getAll();
-  return res.status(200).json({ data });
+  return res.status(200).json({ data })
 });
 
 router.post('/', async (req, res) => {
   const data = await produtoFactory.store(req.body);
-  return res.status(200).json({ data });
+  return res.status(200).json({ data })
 
 
-export default router;
+export default router
 
 });
 
--- src/service/knex.ts
+++ src/service/knex.ts
@@ -12,7 +12,7 @@ export class KnexService {
    } 
 
    return this.conn;
+}
 
-  }
 }
 
